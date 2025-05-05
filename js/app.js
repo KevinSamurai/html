a2e70d88780212d6bca66b5325cc5ebad279d0e8
Kevin Jaramillo Samurai <kjaramillolopez@gmail.com>
2025-05-05 12:21:53 -0500

    primeros ejercicios js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let numero1 =parseInt(prompt("ingrese un número: "))
+let numero2 =parseInt(prompt("ingrese un número: "))
+
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+Let multiplicacion = numero1 * numero2
+let division = numero1 / numero2
+
+
+document.writeln("Suma: " + suma + "<br>")
+document.writeln(´Resta: ${Resta}´)
+document.writeln("Multiplicación: " + multiplicacion + "<br>")
+document.writeln("División: " + division + "<br>")
